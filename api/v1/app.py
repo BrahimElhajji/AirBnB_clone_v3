65a1b28e556fb9cae25f3f698fb883c9301e582a
ismailouzy <ismailouzal@gmail.com>
2024-05-26 10:13:34 -0700

    d

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Handle teardown app context."""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors that returns JSON response."""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
